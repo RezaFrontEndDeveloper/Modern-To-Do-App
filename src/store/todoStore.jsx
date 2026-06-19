71fc6d99a2f8d7cb3be463844a8be0fb7d8348ff
RezaFrontEndDeveloper <rezablackcamel@gmail.com>
2026-06-19 16:48:12 +0330

    fixed save data in local storage

--- src/store/todoStore.jsx
+++ src/store/todoStore.jsx
@@ -1,14 +1,23 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const todoStore = create((set) => ({
-  todos: [],
+const todoStore = create(
+  persist(
+    (set) => ({
+      todos: [],
 
-  toggleTodo: (id, value) =>
-    set((state) => ({
-      todos: state.todos.map((todo) => (todo.id === id ? { ...todo, isDone: value } : todo)),
-    })),
+      toggleTodo: (id, value) =>
+        set((state) => ({
+          todos: state.todos.map((todo) => (todo.id === id ? { ...todo, isDone: value } : todo)),
+        })),
 
-  addTodos: (item) => set((state) => ({ todos: [...state.todos, item] })),
-  deleteItem: (id) => set((state) => ({ todos: state.todos.filter((item) => item.id !== id) })),
-}));
+      addTodos: (item) => set((state) => ({ todos: [...state.todos, item] })),
+      deleteItem: (id) => set((state) => ({ todos: state.todos.filter((item) => item.id !== id) })),
+    }),
+    {
+      name: "local",
+      partialize: (state) => ({ todos: state.todos }),
+    },
+  ),
+);
 export default todoStore;
